a8a036518e92d729a76efe2e03870b225ffb031b
Pushkardeep Khattri <just.pushkardeep@gmail.com>
2025-03-21 14:10:29 +0530

    Added Models

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -9,4 +9,23 @@ public class Player : NetworkBehaviour
     public float pickUpDistance = 50f;
     public float Sensitivity = 100f;
     public Transform holdPosition;
+    public GameObject playerModel;
+
+    private bool IsOnlineMode => NetworkManager.Singleton != null && NetworkManager.Singleton.IsListening;
+
+    private void Start()
+    {
+        if (playerModel == null || !IsOnlineMode) return;
+
+        if (!IsOwner)
+        {
+            playerModel.layer = 0;
+            foreach (Transform child in playerModel.transform)
+            {
+                child.gameObject.layer = 0;
+            }
+        }
+    }
+
+
 }
